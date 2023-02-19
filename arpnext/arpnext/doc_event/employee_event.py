e85b5d9fb84c638596d04c2915ee61a54a22c9e2
Mahmoud Ramadan <mramra1980@gmail.com>
2023-02-19 22:47:40 +0200

    1111

--- arpnext/arpnext/doc_event/employee_event.py
+++ arpnext/arpnext/doc_event/employee_event.py
@@ -3,4 +3,4 @@ import frappe
 def validate_branch(doc,method):
 	if doc.status == 'Left':
 		if not doc.branch:
-			frappe.throw('Must be Enter Reason For Leaving')
+			frappe.throw('Must be Enter Reason For branch')
